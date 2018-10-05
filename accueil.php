a908befb46b737d791c9677b5e0f2d442483adbc
lamineahad <alamineahad28@gmail.com>
2018-10-05 11:26:00 +0000

    2em commit
    avec les fonctionnalite ajout dossier et fichier supprimer

--- accueil.php
+++ accueil.php
@@ -6,101 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <STYLE>
+
+    </STYLE>
 </head>
 <body>
 <?php
-//include 'menu.php';
-/*
-if ($dir = opendir("C:\wamp64\www")) {
-    echo "Pointeur: ".$dir."<br>\n";
-    echo "Chemin: ".getcwd()."<br>\n";
-    while($file = readdir($dir)) {
-        echo "$file<br>\n";
-    }
-    closedir($dir);
-}
-function list_dir($name) {
-    if ($dir = opendir($name)) {
-        while($file = readdir($dir)) {
-            echo "$file<br>\n";
-            if(is_dir($file) && !in_array($file, array(".",".."))) {
-                list_dir($file);
-            }
-        }
-        closedir($dir);
-    }
-}
-list_dir(".");*/
-/*
-function ouvrirdoss(){
-    $dossier = "";
-    if(isset($_GET['dossier'])){
-        $dossier = "C:/wamp64/www/".$_GET['dossier']."/";
-    }else{
-        $dossier="C:/wamp64/www/";
-    }
-  affiche($dossier);
-}
-function affiche($dossierfich)
-{
-
-    // $nb_fichier = 0;
-    echo '<ul>';
-    if ($dossier = opendir($dossierfich)) {
-        while (false !== $fichier = readdir($dossier)) {
-            if ($fichier != '.' && $fichier != "..") {
-                if (is_dir($dossierfich . '' . $fichier)) {
-                    if (isset($_GET['dossier'])) {
-                        ?>
-                        <a href="accueil.php?dossier=<?= $_GET['dossier'] . '/' . $fichier ?>">
-                            <img src="img-dir-open.gif"><?= $fichier ?>
-                        </a>
-                    <?php } else {
-                        ?>
-                        <a href="accueil.php?dossier=<?= $fichier ?>">
-                            <img src="img-dir-open.gif"/>
-                            <?= $fichier ?>
-                        </a>
-                        <?php
-                    }
-                    {
-                        ?>
-                        <img src="img-dir-close.gif"/>
-                        <?= $fichier ?>
-                        <?php
-                    }
-                }
-            }
-            closedir($dossier);
-        }
-        echo '</ul>';
-    }
-}
-
-$nb_fichier++; // On incrémente le compteur de 1
-echo '<li> <img src="img-dir-open.gif"/> <a href="./mondossier/' . $fichier . '">' . $fichier . '</a></li>';
-                        }
-                    else
-                        {
-                            $nb_fichier++; // On incrémente le compteur de 1
-                            echo '<li> <a href="./mondossier/' . $fichier . '">' . $fichier . '</a></li>';
-                        }
-                }
-                else
-                    {
-                        echo '<li> <a href="./mondossier/' . $fichier . '"></a></li>';
-                    }
-            }
-
-        }
-
-        echo '</ul><br />';
-        echo 'Il y a <strong>' . $nb_fichier . '</strong> fichier(s) dans le dossier';
-
-        closedir($dossier);
-    } else echo 'Le dossier n\' a pas pu être ouvert';
-
-}*/
 
 function list_dir($name) {
     if ($dir = opendir($name)) {
@@ -124,25 +35,58 @@ function list_dir($name) {
     }
 }
 if(isset($_GET['dossier'])) {
-    list_dir("../" . $_GET['dossier']);
+    list_dir("./" . $_GET['dossier']);
 }
 else {
-    list_dir("../");
+    list_dir("./");
 }
+
+//creeation de DOSSIER
+    if (isset($_POST['saisi'])){
+        $valeursaisi = $_POST['saisi'];
+        if (!is_dir($valeursaisi)){
+            mkdir($valeursaisi);
+            echo '<script> alert("Votre dossier a ete creer avec succes");</script> ';
+        }else{
+            echo '<script> alert("Erreur votre dossier existe deja");</script> ';
+        }
+
+    }
+//delete dossier
+    if (isset($_POST['saisisup'])){
+        $valeursaisi = $_POST['saisisup'];
+        if (is_dir($valeursaisi)){
+            rmdir($valeursaisi);
+            echo '<script> alert("Votre dossier a ete supprimer avec succes");</script> ';
+        }else{
+            echo '<script> alert("Erreur votre dossier nest pass vue");</script> ';
+        }
+
+    }
+    // creation de fichier
+if (isset($_POST['execut'])){
+    file_put_contents($_POST['saisifich'],"Nouveau fichier");
+}
+
+
 
 
 ?>
+<form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+    <input type="text" name="saisi" id="saisi">
+    <input type="submit" value="Add">
+</form>
+<form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+    <input type="text" name="saisisup" id="saisisup">
+    <input type="submit" value="Delete">
+</form>
 
-<button type="submit" id="nouv" name="nouv" onclick="creerdossier()" >Nouveau</button>
-<script>
-  function creerdossier()
-  {
-      $dossier = 'nouveau_dossier';
-      if(!is_dir($dossier)){
-          mkdir($dossier);
-      }
+<form action="accueil.php" method="post">
+    <input type="text" name="saisifich" id="saisifich">
+    <input type="submit" name="execut" value="Creation fichier">
+</form>
 
-  }
-</script>
+
+
 </body>
 </html>
